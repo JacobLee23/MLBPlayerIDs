c57542b94260edb8b7f586ea1127a917e886e457
Jacob Lee <JLpython@outlook.com>
2022-03-30 15:14:08 -0700

    Package MLBPlayerIDs distribution

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="MLBPlayerIDs",
+    version="0.0.1",
+    author="Jacob Lee",
+    author_email="JLpython@outlook.com",
+    description="A package for scraping IDs for MLB players",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JLpython-py/MLBPlayerIDs",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=setuptools.find_packages(
+        exclude=["mlbides.tests"]
+    ),
+    python_requires=">=3.8",
+)
