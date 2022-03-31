4e87e37015760bcd4fb1b5df0db43df683861244
Jacob Lee <JLpython@outlook.com>
2022-03-30 22:20:22 -0700

    feat: read player ID map data from table as DataFrame

--- mlbids/playeridmap.py
+++ mlbids/playeridmap.py
@@ -3,20 +3,37 @@
 """
 
 import collections
+import datetime
+import math
 import typing
 
 import bs4
+import numpy as np
+import pandas as pd
 import requests
 
 
-class PlayerIDMap:
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+
+def get_soup(url: str) -> bs4.BeautifulSoup:
+    """
+
+    :param url:
+    :return:
+    """
+    res = requests.get(url, headers=HEADERS)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, features="lxml")
+    return soup
+
+
+class SFBBData:
     """
 
     """
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-    }
-
     def __init__(self):
         """
 
@@ -45,9 +62,7 @@ class PlayerIDMap:
 
         :return:
         """
-        res = requests.get(self.base_address, headers=self.headers)
-        soup = bs4.BeautifulSoup(res.text, features="lxml")
-        return soup
+        return get_soup(self.base_address)
 
     @property
     def _element(self) -> bs4.Tag:
@@ -77,3 +92,111 @@ class PlayerIDMap:
         ) = hrefs
 
         return self.URLs(**data)
+
+
+class PlayerIDMap:
+    """
+
+    """
+    _columns = (
+        "PlayerID", "PlayerName", "Birthdate", "FirstName", "LastName",
+        "LastFirstName", "Bats", "Throws", "Team", "League",
+        "Position", "AllPositions", "Active",
+
+        "BaseballHQID",
+        "BaseballProspectusID",
+        "BaseballReferenceID",
+        "CBSID", "CBSName",
+        "ClayDavenportID",
+        "DraftKingsName",
+        "ESPNID", "ESPNName",
+        "FanduelID", "FanduelName",
+        "FanGraphsID", "FanGraphsName",
+        "FantasyProsName",
+        "FantraxID", "FantraxName",
+        "KFFLName",
+        "MasterballName",
+        "MLBID", "MLBName",
+        "NFBCID", "NFBCName", "NFBCLastFirstName",
+        "OttoneuID",
+        "RazzballName",
+        "RetrosheetID",
+        "RotowireName", "RotowireID",
+        "YahooID", "YahooName"
+    )
+    _columns_map = {
+        "IDPLAYER": "PlayerID", "PLAYERNAME": "PlayerName", "BIRTHDATE": "Birthdate",
+        "FIRSTNAME": "FirstName", "LASTNAME": "LastName", "TEAM": "Team",
+        "LG": "League", "POS": "Position", "IDFANGRAPHS": "FanGraphsID",
+        "FANGRAPHSNAME": "FanGraphsName", "MLBID": "MLBID", "MLBNAME": "MLBName",
+        "CBSID": "CBSID", "CBSNAME": "CBSName", "RETROID": "RetrosheetID",
+        "BREFID": "BaseballReferenceID", "NFBCID": "NFBCID", "NFBCNAME": "NFBCName",
+        "ESPNID": "ESPNID", "ESPNNAME": "ESPNName", "KFFLNAME": "KFFLName",
+        "DAVENPORTID": "ClayDavenportID", "BPID": "BaseballProspectusID", "YAHOOID": "YahooID",
+        "YAHOONAME": "YahooName", "MSTRBLLNAME": "MasterballName", "BATS": "Bats",
+        "THROWS": "Throws", "FANTPROSNAME": "FantasyProsName", "LASTCOMMAFIRST": "LastFirstName",
+        "ROTOWIREID": "RotowireID", "FANDUELNAME": "FanduelName", "FANDUELID": "FanduelID",
+        "DRAFTKINGSNAME": "DraftKingsName", "OTTONEUID": "OttoneuID", "HQID": "BaseballHQID",
+        "RAZZBALLNAME": "RazzballName", "FANTRAXID": "FantraxID", "FANTRAXNAME": "FantraxName",
+        "ROTOWIRENAME": "RotowireName", "ALLPOS": "AllPositions", "NFBCLASTFIRST": "NFBCLastFirstName",
+        "ACTIVE": "Active"
+    }
+
+    _integer_columns = [
+        "BaseballHQID", "BaseballProspectusID", "CBSID", "ESPNID", "FanduelID",
+        "MLBID", "NFBCID", "OttoneuID", "RotowireID", "YahooID"
+    ]
+
+    def __init__(self):
+        """
+
+        """
+        self._sfbb = SFBBData()
+
+    @staticmethod
+    def __reformat_birthdate(birthdate: str) -> datetime.datetime:
+        """
+
+        :param birthdate:
+        :return:
+        """
+        try:
+            return datetime.datetime.strptime(birthdate, "%m/%d/%Y")
+        except ValueError:
+            return datetime.datetime.strptime(birthdate, "%m/%d/%y")
+
+    def read_data(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+        url = self._sfbb.urls.web_view
+        res = requests.get(url, headers=HEADERS)
+        soup = get_soup(url)
+
+        title = soup.select_one("div#doc-title > span.name").text
+
+        df = pd.read_html(res.text)[0]
+        df.rename(columns=df.iloc[0], inplace=True)
+        df.drop(
+            index=[df.index[0], df.index[1]], columns=[df.columns[0]],
+            inplace=True
+        )
+        df.dropna(axis=1, how="all", inplace=True)
+        df.rename(columns=self._columns_map, inplace=True)
+        df = df.reindex(columns=self._columns)
+
+        df.loc[:, "Birthdate"] = df.loc[:, "Birthdate"].apply(
+            lambda x: self.__reformat_birthdate(x)
+        )
+        df.loc[:, "AllPositions"] = df.loc[:, "AllPositions"].apply(
+            lambda x: x.split("/")
+        )
+        df.loc[:, "Active"] = df.loc[:, "Active"].apply(
+            lambda x: {"Y": True, "N": False}[x.upper()]
+        )
+        df.loc[:, self._integer_columns] = df.loc[:, self._integer_columns].applymap(
+            lambda x: 0 if math.isnan(float(x)) else int(x)
+        )
+
+        return df
--- mlbids/tests/test_playeridmap.py
+++ mlbids/tests/test_playeridmap.py
@@ -2,55 +2,103 @@
 Unit tests for :py:mod:`mlbids.playerids`.
 """
 
-import bs4
+import datetime
+
+import pandas as pd
 import requests
 
 from mlbids import playeridmap
 
 
-class TestPlayerIDMap:
+class TestSFBBData:
     """
-    Unit tests for :py:class:`mlbids.playerids.PlayerIDMap`.
+    Unit tests for :py:class:`mlbids.playerids.SFBBData`.
     """
-    playerid_map = playerids.PlayerIDMap()
+    sfbb_data = playeridmap.SFBBData()
 
     def test_base_address(self):
         """
-        Unit test for :py:meth:`mlbids.playerids.PlayerIDMap.base_address`.
+        Unit test for :py:meth:`mlbids.playerids.SFBBData.base_address`.
         """
         res = requests.get(
-            self.playerid_map.base_address,
-            headers=self.playerid_map.headers
+            self.sfbb_data.base_address,
+            headers=playeridmap.HEADERS
         )
+        res.raise_for_status()
         assert res.status_code == 200
 
     def test_soup(self):
         """
-        Unit test for :py:meth:`mlbids.playerids.PlayerIDMap._soup`.
+        Unit test for :py:meth:`mlbids.playerids.SFBBData._soup`.
         """
-        res = requests.get(
-            self.playerid_map.base_address,
-            headers=self.playerid_map.headers
-        )
-        soup = bs4.BeautifulSoup(res.text, features="lxml")
+        soup = playeridmap.get_soup(self.sfbb_data.base_address)
         assert soup
 
     def test_element(self):
         """
-        Unit test for :py:meth:`mlbids.playerids.PlayerIDMap._element`.
+        Unit test for :py:meth:`mlbids.playerids.SFBBData._element`.
         """
         css = "div.entry-content > div > table tr:nth-child(2) > td:first-child"
-        assert len(self.playerid_map._soup.select(css)) == 1
+        assert len(self.sfbb_data._soup.select(css)) == 1
 
     def test_urls(self):
         """
-        Unit test for :py:meth:`mlbids.playerids.PlayerIDMap.urls`.
+        Unit test for :py:meth:`mlbids.playerids.SFBBData.urls`.
         :return:
         """
-        elems = self.playerid_map._element.select("a")
+        elems = self.sfbb_data._element.select("a")
         assert len(elems) == 5
         assert all(e.attrs.get("href") for e in elems)
 
-        for url in self.playerid_map.urls:
-            res = requests.get(url, headers=self.playerid_map.headers)
+        for url in self.sfbb_data.urls:
+            res = requests.get(url, headers=playeridmap.HEADERS)
             assert res.status_code == 200
+
+
+class TestPlayerIDMap:
+    """
+    Unit tests for :py:class:`mlbids.playeridmap.PlayerIDMap`.
+    """
+    playerid_map = playeridmap.PlayerIDMap()
+
+    def test_columns(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._columns`.
+        """
+        assert len(playeridmap.PlayerIDMap._columns) == 43
+
+    def test_columns_map(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._columns_map`.
+        """
+        assert len(playeridmap.PlayerIDMap._columns_map) == 43
+        assert set(
+            playeridmap.PlayerIDMap._columns_map.values()
+        ) == set(playeridmap.PlayerIDMap._columns)
+
+    def test_read_data(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_data`.
+        """
+        url = self.playerid_map._sfbb.urls.web_view
+        res = requests.get(url, headers=playeridmap.HEADERS)
+        soup = playeridmap.get_soup(url)
+        assert soup
+
+        assert len(pd.read_html(res.text)) == 1
+        df = self.playerid_map.read_data()
+        assert set(df.columns) == set(self.playerid_map._columns)
+
+        assert all(
+            isinstance(x, datetime.datetime) for x in df.loc[:, "Birthdate"]
+        )
+        assert all(
+            isinstance(x, list) for x in df.loc[:, "AllPositions"]
+        )
+        assert all(
+            isinstance(x, bool) for x in df.loc[:, "Active"]
+        )
+        for col in self.playerid_map._integer_columns:
+            assert all(
+                isinstance(x, int) for x in df.loc[:, col]
+            )
