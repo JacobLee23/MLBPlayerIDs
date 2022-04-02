6b894be563bb14e8f0966a385a67f8da51d9174a
Jacob Lee <JLpython@outlook.com>
2022-04-02 12:57:34 -0700

    feat: update PlayerIDMap class
    - read DataFrame column formatting from JSON data
    - save PlayerIDMap/Changelog CSV/Excel files
    - read PlayerIDMap/Changelog (web view) data

--- mlbids/playeridmap.py
+++ mlbids/playeridmap.py
@@ -4,7 +4,9 @@
 
 import collections
 import datetime
+import json
 import math
+import os
 import typing
 
 import bs4
@@ -97,50 +99,6 @@ class PlayerIDMap:
     """
 
     """
-    _columns = (
-        "PlayerID", "PlayerName", "Birthdate", "FirstName", "LastName",
-        "LastFirstName", "Bats", "Throws", "Team", "League",
-        "Position", "AllPositions", "Active",
-
-        "BaseballHQID",
-        "BaseballProspectusID",
-        "BaseballReferenceID",
-        "CBSID", "CBSName",
-        "ClayDavenportID",
-        "DraftKingsName",
-        "ESPNID", "ESPNName",
-        "FanduelID", "FanduelName",
-        "FanGraphsID", "FanGraphsName",
-        "FantasyProsName",
-        "FantraxID", "FantraxName",
-        "KFFLName",
-        "MasterballName",
-        "MLBID", "MLBName",
-        "NFBCID", "NFBCName", "NFBCLastFirstName",
-        "OttoneuID",
-        "RazzballName",
-        "RetrosheetID",
-        "RotowireName", "RotowireID",
-        "YahooID", "YahooName"
-    )
-    _columns_map = {
-        "IDPLAYER": "PlayerID", "PLAYERNAME": "PlayerName", "BIRTHDATE": "Birthdate",
-        "FIRSTNAME": "FirstName", "LASTNAME": "LastName", "TEAM": "Team",
-        "LG": "League", "POS": "Position", "IDFANGRAPHS": "FanGraphsID",
-        "FANGRAPHSNAME": "FanGraphsName", "MLBID": "MLBID", "MLBNAME": "MLBName",
-        "CBSID": "CBSID", "CBSNAME": "CBSName", "RETROID": "RetrosheetID",
-        "BREFID": "BaseballReferenceID", "NFBCID": "NFBCID", "NFBCNAME": "NFBCName",
-        "ESPNID": "ESPNID", "ESPNNAME": "ESPNName", "KFFLNAME": "KFFLName",
-        "DAVENPORTID": "ClayDavenportID", "BPID": "BaseballProspectusID", "YAHOOID": "YahooID",
-        "YAHOONAME": "YahooName", "MSTRBLLNAME": "MasterballName", "BATS": "Bats",
-        "THROWS": "Throws", "FANTPROSNAME": "FantasyProsName", "LASTCOMMAFIRST": "LastFirstName",
-        "ROTOWIREID": "RotowireID", "FANDUELNAME": "FanduelName", "FANDUELID": "FanduelID",
-        "DRAFTKINGSNAME": "DraftKingsName", "OTTONEUID": "OttoneuID", "HQID": "BaseballHQID",
-        "RAZZBALLNAME": "RazzballName", "FANTRAXID": "FantraxID", "FANTRAXNAME": "FantraxName",
-        "ROTOWIRENAME": "RotowireName", "ALLPOS": "AllPositions", "NFBCLASTFIRST": "NFBCLastFirstName",
-        "ACTIVE": "Active"
-    }
-
     _integer_columns = [
         "BaseballHQID", "BaseballProspectusID", "CBSID", "ESPNID", "FanduelID",
         "MLBID", "NFBCID", "OttoneuID", "RotowireID", "YahooID"
@@ -151,6 +109,53 @@ class PlayerIDMap:
 
         """
         self._sfbb = SFBBData()
+
+    class _DFReformat(typing.NamedTuple):
+        """
+
+        """
+        columns: tuple[str]
+        column_map: dict[str, str]
+
+    @classmethod
+    def __load_reformat(cls, columns_path: str, columnmap_path: str) -> _DFReformat:
+        """
+
+        :param columns_path:
+        :param columnmap_path:
+        :return:
+        """
+        with open(columns_path, "r", encoding="utf-8") as file:
+            columns = json.load(file)
+        with open(columnmap_path, "r", encoding="utf-8") as file:
+            column_map = json.load(file)
+
+        reformat = cls._DFReformat(columns=columns, column_map=column_map)
+        return reformat
+
+    @classmethod
+    def _playeridmap_reformat(cls) -> _DFReformat:
+        """
+
+        :return:
+        """
+        reformat = cls.__load_reformat(
+            os.path.join("mlbids", "data", "playeridmap_columns.json"),
+            os.path.join("mlbids", "data", "playeridmap_columnmap.json")
+        )
+        return reformat
+
+    @classmethod
+    def _changelog_reformat(cls) -> _DFReformat:
+        """
+
+        :return:
+        """
+        reformat = cls.__load_reformat(
+            os.path.join("mlbids", "data", "changelog_columns.json"),
+            os.path.join("mlbids", "data", "changelog_columnmap.json")
+        )
+        return reformat
 
     @property
     def excel_download(self) -> str:
@@ -192,6 +197,45 @@ class PlayerIDMap:
         """
         return self._sfbb.urls.changelog_csv_download
 
+    def save_csv(self, path: str) -> str:
+        """
+
+        :param path:
+        :return:
+        """
+        res = requests.get(self.csv_download, headers=HEADERS)
+
+        with open(path, "wb") as file:
+            file.write(res.content)
+
+        return os.path.abspath(path)
+
+    def save_excel(self, path: str) -> str:
+        """
+
+        :param path:
+        :return:
+        """
+        res = requests.get(self.excel_download, headers=HEADERS)
+
+        with open(path, "wb") as file:
+            file.write(res.content)
+
+        return os.path.abspath(path)
+
+    def save_changelog_csv(self, path: str) -> str:
+        """
+
+        :param path:
+        :return:
+        """
+        res = requests.get(self.changelog_csv_download, headers=HEADERS)
+
+        with open(path, "wb") as file:
+            file.write(res.content)
+
+        return os.path.abspath(path)
+
     @staticmethod
     def __reformat_birthdate(birthdate: str) -> datetime.datetime:
         """
@@ -219,22 +263,19 @@ class PlayerIDMap:
             return False
         raise ValueError
 
-    def read_data(self) -> pd.DataFrame:
+    def _format_playeridmap_df(self, df: pd.DataFrame) -> pd.DataFrame:
         """
 
+        :param df:
         :return:
         """
-        res = requests.get(self.web_view, headers=HEADERS)
-        df = pd.read_html(res.text)[0]
+        reformat = self._playeridmap_reformat()
 
-        df.rename(columns=df.iloc[0], inplace=True)
-        df.drop(
-            index=[df.index[0], df.index[1]], columns=[df.columns[0]],
+        df.rename(
+            index=lambda i: i - df.index[0], columns=reformat.column_map,
             inplace=True
         )
-        df.dropna(axis=1, how="all", inplace=True)
-        df.rename(columns=self._columns_map, inplace=True)
-        df = df.reindex(columns=self._columns)
+        df = df.reindex(columns=reformat.columns)
 
         df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
         df.loc[:, "Birthdate"] = df.loc[:, "Birthdate"].apply(
@@ -249,5 +290,96 @@ class PlayerIDMap:
         df.loc[:, self._integer_columns] = df.loc[:, self._integer_columns].applymap(
             lambda x: 0 if math.isnan(float(x)) else int(x)
         )
+
+        return df
+
+    def read_data(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+        res = requests.get(self.web_view, headers=HEADERS)
+        df = pd.read_html(res.text)[0]
+
+        df.rename(columns=df.iloc[0], inplace=True)
+        df.drop(
+            index=[df.index[0], df.index[1]], columns=[df.columns[0]],
+            inplace=True
+        )
+        df.dropna(axis=1, how="all", inplace=True)
+
+        df = self._format_playeridmap_df(df)
+
+        return df
+
+    def read_csv(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+        res = requests.get(self.csv_download, headers=HEADERS)
+        temp_path = f"temp-PlayerIDMap-{datetime.datetime.now().microsecond}.csv"
+        with open(temp_path, "wb") as file:
+            file.write(res.content)
+
+        try:
+            df = pd.read_csv(temp_path)
+        finally:
+            os.remove(os.path.abspath(temp_path))
+
+        df = self._format_playeridmap_df(df)
+
+        return df
+
+    def _format_changelog_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param df:
+        :return:
+        """
+        reformat = self._changelog_reformat()
+
+        df.rename(
+            index=lambda i: i - df.index[0], columns=reformat.column_map,
+            inplace=True
+        )
+        df = df.reindex(columns=reformat.columns)
+        df.loc[:, "Date"] = df.loc[:, "Date"].apply(
+            lambda x: datetime.datetime.strptime(x, "%m/%d/%Y")
+        )
+
+        return df
+
+    def read_changelog_data(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+        res = requests.get(self.changelog_web_view, headers=HEADERS)
+        df = pd.read_html(res.text)[0]
+
+        df.rename(columns=df.iloc[0], inplace=True)
+        df.drop(index=df.index[0], inplace=True)
+
+        df = self._format_changelog_df(df)
+
+        return df
+
+    def read_changelog_csv(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+        res = requests.get(self.changelog_csv_download, headers=HEADERS)
+        temp_path = f"temp-Changelog-{datetime.datetime.now().microsecond}.csv"
+        with open(temp_path, "wb") as file:
+            file.write(res.content)
+
+        try:
+            df = pd.read_csv(temp_path)
+        finally:
+            os.remove(os.path.abspath(temp_path))
+
+        df = self._format_changelog_df(df)
 
         return df
--- mlbids/tests/test_playeridmap.py
+++ mlbids/tests/test_playeridmap.py
@@ -3,6 +3,7 @@ Unit tests for :py:mod:`mlbids.playerids`.
 """
 
 import datetime
+import os
 
 import pandas as pd
 import pytest
@@ -62,6 +63,44 @@ class TestPlayerIDMap:
     """
     playerid_map = playeridmap.PlayerIDMap()
 
+    def test_playeridmap_reformat(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._playeridmap_reformat`.
+        """
+        paths = [
+            os.path.join("mlbids", "data", "playeridmap_columns.json"),
+            os.path.join("mlbids", "data", "playeridmap_columnmap.json")
+        ]
+        assert all(os.path.exists(p) for p in paths)
+
+        reformat = self.playerid_map._playeridmap_reformat()
+        assert len(reformat.columns) == len(reformat.column_map) == 43
+        assert all(isinstance(x, str) for x in reformat.columns)
+        assert all(
+            isinstance(a, str) and isinstance(b, str)
+            for a, b in reformat.column_map.items()
+        )
+        assert all(x.isupper() for x in reformat.column_map.keys())
+
+    def test_changelog_reformat(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._changelog_reformat`.
+        """
+        paths = [
+            os.path.join("mlbids", "data", "changelog_columns.json"),
+            os.path.join("mlbids", "data", "changelog_columnmap.json")
+        ]
+        assert all(os.path.exists(p) for p in paths)
+
+        reformat = self.playerid_map._changelog_reformat()
+        assert len(reformat.columns) == len(reformat.column_map) == 2
+        assert all(isinstance(x, str) for x in reformat.columns)
+        assert all(
+            isinstance(a, str) and isinstance(b, str)
+            for a, b in reformat.column_map.items()
+        )
+        assert all(x.isupper() for x in reformat.column_map.keys())
+
     @pytest.mark.parametrize(
         "url", [
             playerid_map.excel_download,
@@ -85,32 +124,13 @@ class TestPlayerIDMap:
         res = requests.get(url, headers=playeridmap.HEADERS)
         assert res.status_code == 200, url
 
-    def test_columns(self):
+    @pytest.mark.parametrize(
+        "df", [playerid_map.read_data()]
+    )
+    def test_format_playeridmap_df(self, df: pd.DataFrame):
         """
-        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._columns`.
+        Unit test for ;py:meth:`mlbids.playeridmap.PlayerIDMap._format_playeridmap_df`.
         """
-        assert len(playeridmap.PlayerIDMap._columns) == 43
-
-    def test_columns_map(self):
-        """
-        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._columns_map`.
-        """
-        assert len(playeridmap.PlayerIDMap._columns_map) == 43
-        assert set(
-            playeridmap.PlayerIDMap._columns_map.values()
-        ) == set(playeridmap.PlayerIDMap._columns)
-
-    def test_read_data(self):
-        """
-        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_data`.
-        """
-        url = self.playerid_map._sfbb.urls.web_view
-        res = requests.get(url, headers=playeridmap.HEADERS)
-        assert len(pd.read_html(res.text)) == 1
-
-        df = self.playerid_map.read_data()
-        assert set(df.columns) == set(self.playerid_map._columns)
-
         assert all(
             isinstance(x, datetime.datetime) for x in df.loc[:, "Birthdate"]
         )
@@ -124,3 +144,50 @@ class TestPlayerIDMap:
             assert all(
                 isinstance(x, int) for x in df.loc[:, col]
             )
+
+    def test_read_data(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_data`.
+        """
+        url = self.playerid_map._sfbb.urls.web_view
+        res = requests.get(url, headers=playeridmap.HEADERS)
+        assert len(pd.read_html(res.text)) == 1
+
+        df = self.playerid_map.read_data()
+        assert set(df.columns) == set(self.playerid_map._playeridmap_reformat().columns)
+
+    def test_read_csv(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_csv`.
+        """
+        df = self.playerid_map.read_csv()
+        assert all(df == self.playerid_map.read_data())
+
+    @pytest.mark.parametrize(
+        "df", [playerid_map.read_changelog_data()]
+    )
+    def test_format_changelog_df(self, df: pd.DataFrame):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap._format_changelog_df`.
+        """
+        assert all(
+            isinstance(x, datetime.datetime) for x in df.loc[:, "Date"]
+        )
+
+    def test_read_changelog_data(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_changelog_data`.
+        """
+        url = self.playerid_map.changelog_web_view
+        res = requests.get(url, headers=playeridmap.HEADERS)
+        assert len(pd.read_html(res.text)) == 1
+
+        df = self.playerid_map.read_changelog_data()
+        assert set(df.columns) == set(self.playerid_map._changelog_reformat().columns)
+
+    def test_read_changelog_csv(self):
+        """
+        Unit test for :py:meth:`mlbids.playeridmap.PlayerIDMap.read_changelog_csv`.
+        """
+        df = self.playerid_map.read_changelog_csv()
+        assert all(df == self.playerid_map.read_changelog_data())
