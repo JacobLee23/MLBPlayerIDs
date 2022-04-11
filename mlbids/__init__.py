eb949253fca07b1ce4461b0206be20bda3c14f91
Jacob Lee <JLpython@outlook.com>
2022-04-11 11:48:41 -0700

    fix: AttributeError raised when retrieving DataFrame ID data

--- mlbids/__init__.py
+++ mlbids/__init__.py
@@ -10,31 +10,24 @@ import pandas as pd
 from .playeridmap import PlayerIDMap
 
 
-def _site(columns: list[str]) -> typing.Callable:
+def _site_property(columns: list[str]):
     """
 
     :param columns:
     :return:
     """
-    def decorator(method: typing.Callable) -> typing.Callable:
-        """
+    def getter(self):
+        general = self.__getattribute__("general")
+        data = self.__getattribute__("data")
 
-        :param method:
-        :return:
-        """
-        def wrapper(self) -> pd.DataFrame:
-            """
+        if not isinstance(general, pd.DataFrame):
+            raise TypeError
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError
 
-            :param self:
-            :return:
-            """
-            if "general" not in self.__dict__ or not isinstance(self.general, pd.DataFrame):
-                raise AttributeError("object %r has no attribute 'general'" % self)
-            if "data" not in self.__dict__ or not isinstance(self.data, pd.DataFrame):
-                raise AttributeError("object %r has no attribute 'data'" % self)
-            return self.general.join(self.data.loc[:, columns])
-        return wrapper
-    return decorator
+        return general.join(data.loc[:, columns])
+    
+    return property(getter)
 
 
 class PIDMap:
@@ -47,7 +40,7 @@ class PIDMap:
         self._changelog = self.pid_map.read_changelog_data()
 
         self.__info = [
-            "Last Name", "First Name", "PlayerName", "LastFirstName", "Birthdate", "PlayerID"
+            "LastName", "FirstName", "PlayerName", "LastFirstName", "Birthdate", "PlayerID"
         ]
         self.__general = [
             "Bats", "Throws", "Team", "League", "Position", "AllPositions", "Active"
@@ -93,42 +86,25 @@ class PIDMap:
         """
         return self.info.join(self.data.loc[:, self.__general])
 
-    @staticmethod
-    def __site_property(columns: list[str], docstring: typing.Optional[str] = None):
-        """
-
-        :param columns:
-        :param docstring:
-        :return:
-        """
-        def getter(self):
-            if "general" not in self.__dict__ or not isinstance(self.general, pd.DataFrame):
-                raise AttributeError("object %r has no attribute 'general'" % self)
-            if "data" not in self.__dict__ or not isinstance(self.data, pd.DataFrame):
-                raise AttributeError("object %r has no attribute 'data'" % self)
-            return self.general.join(self.data.loc[:, columns])
-
-        return property(getter, doc=docstring)
-
-    baseballhq = __site_property(["BaseballHQID"])
-    baseball_prospectus = __site_property(["BaseballProspectusID"])
-    cbs = __site_property(["CBSID", "CBSName"])
-    clay_davenport = __site_property(["ClayDavenportID"])
-    draft_kings = __site_property(["DraftKingsName"])
-    espn = __site_property(["ESPNID", "ESPNName"])
-    fanduel = __site_property(["FanduelID", "FanduelName"])
-    fangraphs = __site_property(["FanGraphsID", "FanGraphsName"])
-    fantasy_pros = __site_property(["FantasyProsName"])
-    fantrax = __site_property(["FantraxName"])
-    kffl = __site_property(["KFFLName"])
-    masterball = __site_property(["MasterballName"])
-    mlb = __site_property(["MLBID", "MLBName"])
-    nfbc = __site_property(["NFBCID", "NFBCName", "NFBCLastFirstName"])
-    ottoneu = __site_property(["OttoneuID"])
-    razzball = __site_property(["RazzballName"])
-    retrosheet = __site_property(["RetrosheetID"])
-    rotowire = __site_property(["RotowireID", "RotowireName"])
-    yahoo = __site_property(["YahooID", "YahooName"])
+    baseballhq = _site_property(["BaseballHQID"])
+    baseball_prospectus = _site_property(["BaseballProspectusID"])
+    cbs = _site_property(["CBSID", "CBSName"])
+    clay_davenport = _site_property(["ClayDavenportID"])
+    draft_kings = _site_property(["DraftKingsName"])
+    espn = _site_property(["ESPNID", "ESPNName"])
+    fanduel = _site_property(["FanduelID", "FanduelName"])
+    fangraphs = _site_property(["FanGraphsID", "FanGraphsName"])
+    fantasy_pros = _site_property(["FantasyProsName"])
+    fantrax = _site_property(["FantraxName"])
+    kffl = _site_property(["KFFLName"])
+    masterball = _site_property(["MasterballName"])
+    mlb = _site_property(["MLBID", "MLBName"])
+    nfbc = _site_property(["NFBCID", "NFBCName", "NFBCLastFirstName"])
+    ottoneu = _site_property(["OttoneuID"])
+    razzball = _site_property(["RazzballName"])
+    retrosheet = _site_property(["RetrosheetID"])
+    rotowire = _site_property(["RotowireID", "RotowireName"])
+    yahoo = _site_property(["YahooID", "YahooName"])
 
     @property
     def last_update(self) -> datetime.datetime:
